8ce7caee234454591a6b599d7d80f6c8112b2bba
Matthias Claw <matthias@scalable.so>
2026-09-21 10:03:17 +0200

    test: compile PTY pixel geometry check on macOS

--- src/pty/fd.rs
+++ src/pty/fd.rs
@@ -253,8 +253,8 @@ mod tests {
                 &mut master,
                 &mut slave,
                 std::ptr::null_mut(),
-                std::ptr::null(),
-                std::ptr::null(),
+                std::ptr::null_mut(),
+                std::ptr::null_mut(),
             )
         };
         assert_eq!(
